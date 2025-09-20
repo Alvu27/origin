11fe1235f5efb8a5b301ab9e846862f81b7ca734
Alvu27 <s208012compug21@gmail.com>
2025-09-20 19:01:23 +0530

    Add files via upload

---
+++ package.py
@@ -0,0 +1,6 @@
+days = ['Monday','Tuesday','Wednesday',
+        'Thursday','Friday']
+print(days)
+paragraph = """This is a paragraph.
+ It is made up of multiple lines and sentences."""
+ print(paragraph)
